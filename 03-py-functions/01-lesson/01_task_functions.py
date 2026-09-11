9626ef25a8cf019faf79dea97ae01e62101874fb
Viktoriia Protasova <protasova.dev@gmail.com>
2026-09-11 14:34:24 +0200

    Rename folder structure.

---
+++ 03-py-functions/01-lesson/01_task_functions.py
@@ -0,0 +1,17 @@
+#%%
+def show_welcome():
+    print("Welcome to the course!")
+    print("Let us practice Python!")
+
+show_welcome()
+show_welcome()
+
+#%%
+def show_messages():
+   print("Inside the function")
+   print("Still inside the function")
+
+print("Before calling show_messages")
+show_messages()
+print("After calling show_messages")
+
